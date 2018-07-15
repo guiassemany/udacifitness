52c81fd58426ef5d5ebc102626a16599bc5d4b9f
Guilherme Assemany <guilhermeassemany@hotmail.com>
2018-07-15 19:50:06 -0300

    UdaciFitness start

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/addEntry';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    );
+  }
+}
